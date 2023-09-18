ee612fd5e0b30ab8aa5bd8095ab1a97f5a560f09
Iván Sánchez Corral <i.sanchezc@outlook.com>
2023-09-18 11:23:27 +0200

    Added sqlite initialization script

---
+++ src/sql/init_db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create FileMetadata table in secure-file-sharing-backend.db
+sqlite3 ../../secure-file-sharing-backend.db <<EOF
+-- Create FileMetadata table
+CREATE TABLE IF NOT EXISTS FileMetadata (
+    id TEXT PRIMARY KEY,
+    originalFilename TEXT NOT NULL,
+    fileSize INTEGER NOT NULL,
+    key BLOB NOT NULL,
+    iv BLOB NOT NULL,
+    expiresAt TEXT,
+    downloadCount INTEGER,
+    maxDownloadCount INTEGER
+);
+
+EOF
